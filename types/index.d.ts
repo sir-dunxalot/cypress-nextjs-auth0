1961604a9f71b507823865878fb9cd0a313b5be4
Nikolas LeBlanc <nikolas.leblanc@gmail.com>
2021-09-17 11:37:39 -0400

    Update index.d.ts

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,5 +1,11 @@
+interface loginCredentials {
+  username?: string,
+  password?: string
+}
+
 declare namespace Cypress {
   interface Chainable<Subject = any> {
+    login(loginCredentials): Chainable<Element>;
     login(): Chainable<Element>;
     logout(): Chainable<Element>;
   }
