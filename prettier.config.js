11b742ae0f91c92e203cebeb80231a12e33a99b3
Stefan Natter <stefan@natter.at>
2022-02-11 20:45:13 +0100

    chore: added prettier

---
+++ prettier.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  arrowParens: 'avoid',
+  bracketSpacing: true,
+  endOfLine: 'auto',
+  printWidth: 80,
+  proseWrap: 'always',
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  tabWidth: 2,
+  useTabs: false,
+};
