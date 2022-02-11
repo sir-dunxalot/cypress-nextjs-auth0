c00485fa18c07c8107ad91143d814bccc2376dcc
Stefan Natter <stefan@natter.at>
2022-02-11 19:23:48 +0100

    refactor: applied PR feedback

--- src/commands/preserve-auth0-cookies-once.js
+++ src/commands/preserve-auth0-cookies-once.js
@@ -13,6 +13,9 @@ Cypress.Commands.add('preserveAuth0CookiesOnce', () => {
     // We assume that the cookie is never splitted into more than 10 parts
     ...Array(10)
       .fill(1)
-      .map((x, y) => `${Cypress.env('auth0SessionCookieName')}.${x + y}`),
+      .map(
+        (value, index) =>
+          `${Cypress.env('auth0SessionCookieName')}.${value + index}`,
+      ),
   );
 });
